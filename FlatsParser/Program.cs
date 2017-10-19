e22c22ce17a90229ba130b5f0dfed085ee5292a6
Artur Shaikhutdinov <rut141@yandex.ru>
2017-10-19 22:28:47 +0400

    Add project files.

---
+++ FlatsParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatsParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
