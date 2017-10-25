37da925b8afbd30b5a0d229494ee8daab0996911
Artur Shaikhutdinov <rut141@yandex.ru>
2017-10-25 23:29:35 +0400

    Added parallel processing

--- FlatsParser/Program.cs
+++ FlatsParser/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 
@@ -9,21 +9,30 @@ namespace FlatsParser
 {
     internal class Program
     {
+        private const int FirstId = 1449;
+        private const int LastId = 1739;
+        private const int FlatsCount = LastId - FirstId;
+
         public static void Main(string[] args)
         {
-            var client = new WebClient { Encoding = Encoding.UTF8 };
-            var flats = new List<Flat>();
-            for (var id = 1449; id < 1739; id++)
-            {
-                var url = $"http://www.xn--80adrpkbapik.xn--p1ai/flats/{id}";
-                var downloadString = client.DownloadString(url);
-                var flat = RegexPageParser.ParsePage(downloadString);
-                flat.Id = id;
-                flat.Url = url;
+            
+            var flatIds = Enumerable.Range(FirstId, FlatsCount);
 
-                flats.Add(flat);
-                Console.WriteLine($"Added {id}");
-            }
+            var flats = flatIds
+                .AsParallel().Select(id =>
+            {
+                using (var client = new WebClient {Encoding = Encoding.UTF8})
+                {
+                    var url = $"http://www.xn--80adrpkbapik.xn--p1ai/flats/{id}";
+                    var downloadString = client.DownloadString(url);
+                    var flat = RegexPageParser.ParsePage(downloadString);
+                    flat.Id = id;
+                    flat.Url = url;
+                    Console.WriteLine($"Added {id}");
+                    return flat;
+                }
+            }).ToList();
+
             Console.WriteLine("Sorting...");
             flats.Sort((flat, flat1) => flat1.Number - flat.Number);
             Console.WriteLine("Sorted");
