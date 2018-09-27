f4665b67caa2dae2c1502c0213fcb7ad12a7d73d
Artur Shaikhutdinov <ashaikhutdinov@skbkontur.ru>
2018-09-27 22:26:17 +0400

    NewModelParser: add lost files

---
+++ FlatsParser/FlatsDistinct.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace FlatsParser
+{
+	public class FlatsDistinct
+	{
+		public FlatsDistinct([CanBeNull] Flat latestState = null, [CanBeNull] Flat previousState = null)
+		{
+			LatestState = latestState;
+			PreviousState = previousState;
+		}
+
+		[CanBeNull]
+		public Flat LatestState { get; }
+		[CanBeNull]
+		public Flat PreviousState { get; }
+	}
+}
